fec35904e4e24c765e05fb7860f1bbbef7e50324
arkiver <arkiver@protonmail.com>
2022-04-09 02:52:36 +0200

    Version 20220409.05. Reduce multi item size to 4.

--- pipeline.py
+++ pipeline.py
@@ -59,11 +59,11 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20220409.04'
+VERSION = '20220409.05'
 USER_AGENT = 'Archive Team'
 TRACKER_ID = 'telegram'
 TRACKER_HOST = 'legacy-api.arpa.li'
-MULTI_ITEM_SIZE = 20
+MULTI_ITEM_SIZE = 4
 
 
 ###########################################################################
