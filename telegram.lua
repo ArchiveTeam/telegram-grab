351810211ca2e44b0375f7c3ed47266da12a5b32
arkiver <arkiver@protonmail.com>
2022-07-05 17:43:05 +0200

    Version 20220705.04. Prevent retrying on discussion page where comments are supported but not made yet.

--- telegram.lua
+++ telegram.lua
@@ -682,6 +682,7 @@ wget.callbacks.write_to_warc = function(url, http_stat)
           not string.match(html, '<div%s+class="tme_no_messages_found">')
           or string.match(html, '<div%s+class="tme_no_messages_found">Discussion%s+is not%s+available%s+at the%s+moment%.')
           or string.match(html, '<div%s+class="tme_no_messages_found">Please%s+open%s+Telegram%s+to%s+view%s+this%s+discussion%s+from')
+          or string.match(html, '<h3%s+class="tgme_post_discussion_header">%s*<span%s+class="js%-header">Comments</span>%s+on%s+<a%s+href="https?://t%.me/[^/]+/[0-9]+">this%s+post</a>%s*</h3>')
         )
       ) and not (
         url["url"] == api_url
