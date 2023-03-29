2da094fb96079bec5587350266e75e132a743d77
arkiver <arkiver@protonmail.com>
2023-03-29 15:00:56 +0200

    Version 20230329.01. Support tgsticker.js version 31.

--- pipeline.py
+++ pipeline.py
@@ -62,7 +62,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20230325.02'
+VERSION = '20230329.01'
 USER_AGENT = 'Archive Team'
 TRACKER_ID = 'telegram'
 TRACKER_HOST = 'legacy-api.arpa.li'
