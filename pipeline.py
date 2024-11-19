832fada7eae829669369225b4388c3ca275e8346
arkiver <arkiver@protonmail.com>
2024-11-19 13:03:15 +0100

    Version 20241119.01. Set minimum Wget-AT version at 1.21.3-at.20241119.01.

--- pipeline.py
+++ pipeline.py
@@ -56,7 +56,7 @@ WGET_AT = find_executable(
     'Wget+AT',
     HigherVersion(
         r'(GNU Wget 1\.[0-9]{2}\.[0-9]{1}-at\.[0-9]{8}\.[0-9]{2})[^0-9a-zA-Z\.-_]',
-        'GNU Wget 1.21.3-at.20230623.01'
+        'GNU Wget 1.21.3-at.20241119.01'
     ),
     [
         './wget-at',
@@ -73,7 +73,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20240914.01'
+VERSION = '20241119.01'
 USER_AGENT = 'Archive Team'
 TRACKER_ID = 'telegram'
 TRACKER_HOST = 'legacy-api.arpa.li'
