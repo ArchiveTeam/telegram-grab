3bc22f92aa5d9eb9d70fd804ea0f58d6e31447bd
arkiver <arkiver@protonmail.com>
2025-07-24 16:47:01 +0200

    Version 20250724.01. Update widget-frame.js version to 65.

--- telegram.lua
+++ telegram.lua
@@ -63,7 +63,7 @@ local disco_current_url = nil
 local retry_url = false
 
 local current_js = {
-  ["widget-frame.js"] = "64",
+  ["widget-frame.js"] = "65",
   ["tgwallpaper.min.js"] = "3",
   ["tgsticker.js"] = "31",
   ["telegram-web.js"] = "14",
