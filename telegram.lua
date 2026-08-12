be2f05609497c9176d56cfe9baeeccf1660c7df8
arkiver <arkiver@protonmail.com>
2026-08-12 19:51:15 +0200

    Version 20260812.01. upport widget-frame.js 68.

--- telegram.lua
+++ telegram.lua
@@ -63,7 +63,7 @@ local disco_current_url = nil
 local retry_url = false
 
 local current_js = {
-  ["widget-frame.js"] = "67",
+  ["widget-frame.js"] = "68",
   ["tgwallpaper.min.js"] = "3",
   ["tgsticker.js"] = "32",
   ["telegram-web.js"] = "14",
